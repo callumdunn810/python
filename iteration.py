ced40caeae0b4302d1aec3f847cae99b43d0b494
Callum Dunn <callumdunn810@gmail.com>
2021-06-09 13:03:29 +0000

    update

--- iteration.py
+++ iteration.py
@@ -1,28 +1,53 @@
-#number = 1
+# number = 1
 
-#while number < 100:
-    #print(number)
-    #number +=1
+# while number < 100:
+#     print(number)
+#     number +=1
 
-#print(number)
+# print(number)
 
-# people = [ "Callum", "Ben", "Victoria"]
+# people = [ "Callum", "Ben", "Victoria","Liana","Luke"]
 
-#for person in  people:
-    #print(person)
+# for person in  people:
+#     print(person)
 
-#my_range = range(10, 100, 2)
-#print(list(my_range))
+# my_range = range(10, 100, 2)
+# print(list(my_range))
 
-#for number in range(10):
-    #print(number)
+# for number in range(10):
+#     print(number)
 
-count = 0 
-name = str(input("What is your name?"))
+# count = 0 
+# name = str(input("What is your name?"))
 
-while count < 5:
-    print(name, "is awesome!")
+# while count < 5:
+#     print(name, "is awesome!")
+#     count +=1
+
+# for i in range(1,11):
+#     print(i)
+
+count = 0
+
+name1 = str(input("What is your name?"))
+name2 = str(input("What is your name?"))
+name3 = str(input("What is your name?"))
+name4 = str(input("What is your name?"))
+name5 = str(input("What is your name?"))
+
+
+while count < 1:
+    print(name1, "is cool")
     count +=1
-
-for i in range(1,11):
-    print(i)
+while count < 1:
+    print(name2, "is cool")
+    count +=1
+while count < 1:
+    print(name3, "is cool")
+    count += 1
+while count < 1:
+    print(name4, "is cool")
+    count +=1
+while count < 1:
+    print(name5, "is cool")
+    count +=1
